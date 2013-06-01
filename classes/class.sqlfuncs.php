57329d6be8eeb2d721b6addb0bdacb9475dd8d3b
Karl Kloppenborg <karl@inventionlabs.com.au>
2013-06-01 17:13:22 +1000

    SQL Construct completed

--- classes/class.sqlfuncs.php
+++ classes/class.sqlfuncs.php
@@ -9,6 +9,14 @@ namespace whmcsDataGen;
 
 
 class sqlCore {
+	
+	/**
+	 * Private variable assignments
+	 */
+	private $_sqlObject; //Store the SQL object in here.
+	private $_errorHandler; //Store any errors to throw an exception if needed.
+
+
 	/**
 	 * When instanciating the class we need to connect to the WHMCS installation, lets get the configuration file
 	 * @author Karl Kloppenborg
@@ -18,6 +26,16 @@ class sqlCore {
 		//Check for existence;
 		if(!file_exists($configuration))
 			throw new exception(sprintf("Unable to open configuration file located at: %s", $configuration));
-		
+		//Get here - file exists, lets use it.
+		include_once($configuration); //Include the configuration file so we can use the DB login details.
+
+		//Okay lets now instanciate the SQL object.
+		$this->_sqlObject = new mysqli($db_host, $db_username, $db_password);
+		//check connection
+		if(mysqli_connect_errno())
+			throw new exception(sprintf("MySQLi Unable to connect for reason: %s", mysqli_connect_error()));
+		//Set the charset
+		if(!$this->_sqlObject->set_charset($mysql_charset))
+			throw new exception(sprintf("Unable to set the MySQLi Char Set to: %s", $mysql_charset));
 	}
 }
